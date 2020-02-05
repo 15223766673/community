c734d926a9b7ab7d892e938a47e5caa72a8b5450
TianZhou <761702168@QQ.COM>
2020-02-05 22:19:46 +0800

    init repo

---
+++ src/main/java/life/majiang/communty/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package life.majiang.communty.community.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name",required = false,defaultValue = "name") String name, Model model){
+
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+}
